6fe52ecb73028da5e76a2d04f7d237cfe4da30aa
Riccardo Elena <witherheart63@gmail.com>
2023-11-29 12:18:54 +0100

    fix(oo): fixed missing pacakge-info file for test folder.

---
+++ oo/uninadelivery/src/test/java/com/unina/_2324oobd/_22/package-info.java
@@ -0,0 +1,7 @@
+
+/**
+ * This package contains test classes for the UniNa Delivery project.
+ * It provides functionalities for testing the delivery management and package
+ * tracking.
+ */
+package com.unina._2324oobd._22;
