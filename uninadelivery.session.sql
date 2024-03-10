57544e19979bafba05974cbb8729ca8f59cc8753
Riccardo Elena <witherheart63@gmail.com>
2024-03-10 15:45:03 +0100

    docs(oo): written documentation for the project. Still missing class diagrams

--- uninadelivery.session.sql
+++ uninadelivery.session.sql
@@ -1,49 +1,23 @@
-SELECT *
+select *
 from account
-ORDER BY RANDOM()
-LIMIT 1;
-SELECT *
-from "Order";
-begin;
-SELECT setval(
-    '"Order_orderid_seq"',
-    (
-      SELECT MAX(orderid)
-      FROM "Order"
-    )
-  );
-INSERT INTO "Order" (email, name, supplier, quantity)
-SELECT a.email,
-  s.name,
-  s.supplier,
-  CEIL(s.quantity * RANDOM())
-FROM account a,
-  stores s
-where EXISTS (
-    SELECT 1
-    from deposit
-    WHERE depositid = s.depositid
-      and isSameCity(a.zipcode, a.country, zipcode, country)
-  )
-ORDER BY RANDOM()
-LIMIT 20;
-UPDATE "Order"
-set extrawarranty = FLOOR(6 * RANDOM())
-where orderid >= 53;
-SELECT *
-from "Order";
-rollback;
-update "Order"
-set isExpress = true
-where orderid in (
-    select orderid
-    from "Order"
-    where orderid >= 53
-    order by RANDOM()
-    limit 10
-  );
-SELECT *
-from "Order";
-update "Order"
-set iscompleted = FALSE
-where orderid = 1
+where email = 'giorgionecapitone@gmail.com'
+create or replace function isDirectionCorrect(
+    zc area.zipcode %type,
+    cy area.country %type,
+    sdate date,
+    tid transport.transportid %type
+  ) returns boolean as $$ begin return case
+    when exists (
+      select 1
+      from covers
+      where (
+          zipcode <> zc
+          or country <> cy
+        )
+        and date = sdate
+        and transportid = tid
+    ) then false
+    else true
+  end;
+end;
+$$ language plpgsql;
